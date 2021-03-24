b8ab5c6262e3051d8c7789717f779e544a27636f
sirgentleman <imnotfreeman@gmail.com>
2021-03-24 17:03:39 +0100

    add basic image class

--- app/image/image.py
+++ app/image/image.py
@@ -1,4 +1,6 @@
-"""Container for processed image."""
+"""Support for containing image."""
+
+import numpy
 
 class RawDataContainer:
     """Structure for file data and communication."""
@@ -18,3 +20,25 @@ class RawDataContainer:
         
         except EnvironmentError as err:
             raise Exception('Error occured while trying to read from file {}'.format(file_path))
+
+
+class Image(RawDataContainer):
+    """Container for image data."""
+
+    def __init__(self, data_buffer, color_format=None, processed_data=None):
+        RawDataContainer.__init__(self,data_buffer=data_buffer)
+        self.color_format = color_format
+        self.processed_data = processed_data
+
+
+    @property
+    def height(self):
+        if(self.processed_data == None):
+            return None
+        return self.processed_data.shape()[0]
+
+    @property
+    def width(self):
+        if(self.processed_data == None):
+            return None
+        return self.processed_data.shape()[1]
