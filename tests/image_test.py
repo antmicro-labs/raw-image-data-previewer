02ab9c4a77bb8aa4fe1b34733bb7ac0553ed3d5f
Błażej Ułanowicz <imnotfreeman@gmail.com>
2021-04-08 16:18:13 +0200

    Add data parsers from raw to target format and get displayable (#14)
    * add data parsing utilities
    
    * change width and height access
    
    * add parsers test
    
    * remove cutom lcm method

--- tests/image_test.py
+++ tests/image_test.py
@@ -12,7 +12,7 @@ class TestImageClass(unittest.TestCase):
         self.empty_img = image.Image(None)
         with open(self.TEST_FILE_BGR, "rb") as file:
             self.img = image.Image(file.read(), cf.BGR3_FORMAT,
-                                   numpy.zeros((1280, 720, 3)))
+                                   numpy.zeros((720, 1280, 3)))
 
     def test_from_file(self):
         self.assertEqual(
