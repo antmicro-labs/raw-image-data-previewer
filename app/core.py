a1ad68152713073aa62c7c5de1b9dbf621bbcc10
Dawid Dopart <61831647+DopartDawid@users.noreply.github.com>
2021-04-21 18:17:52 +0200

    Refactor of color_formats and definition of new one, core update (#22)
    * Refactor color_format, core, main
    
    * Add RGB32 RGB332 RGB565 format definition

--- app/core.py
+++ app/core.py
@@ -1,33 +1,38 @@
 """Main functionalities."""
 
 from .image.image import (Image, RawDataContainer)
-from .image.color_format import (RGB3_FORMAT, BGR3_FORMAT)
+from .image.color_format import AVAILABLE_FORMATS
 from .parser.factory import ParserFactory
 
 
-def run_core_functionality(args):
-
-    image = create_Image(args)
-    parser = ParserFactory.create_object(
-        determine_color_format(args["color_format"]))
+def load_image(file_path, color_format, resolution):
+    try:
+        image = Image.from_file(file_path)
+        parser = ParserFactory.create_object(
+            determine_color_format(color_format))
+    except Exception as e:
+        print(type(e).__name__, e)
 
     image = parser.parse(image.data_buffer,
-                         determine_color_format(args["color_format"]),
-                         args["resolution"][0], args["resolution"][1])
-    displayable_data = parser.get_displayable(image)
+                         determine_color_format(color_format), resolution[0],
+                         resolution[1])
+
+    return image
 
 
-def create_Image(args):
+def get_displayable(image):
 
-    raw_data = RawDataContainer.from_file(args["FILE_PATH"])
-    return Image(raw_data.data_buffer)
+    if image.color_format is None:
+        raise Exception("Image should be already parsed!")
+    parser = ParserFactory.create_object(image.color_format)
+
+    return parser.get_displayable(image)
 
 
 def determine_color_format(format_string):
 
-    if format_string == "RGB3":
-        return RGB3_FORMAT
-    elif format_string == "BGR3":
-        return BGR3_FORMAT
+    if format_string in AVAILABLE_FORMATS.keys():
+        return AVAILABLE_FORMATS[format_string]
     else:
-        raise NotImplementedError
+        raise NotImplementedError(
+            "Provided string is not name of supported format.")
